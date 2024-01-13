3305d988616857a493886a765d0db8791d5aa2ee
Pedro Juan Royo <pedro.juan.royo@gmail.com>
2024-01-13 19:07:17 +0000

    variables section done

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    /* This code does not compile because we are changing 
+    /* This code does not compile because we are changing
      * an immutable variable.
     let x = 5;
     println!("x is: {x}");
@@ -17,4 +17,16 @@ fn main() {
     const ONE_HOUR_IN_MINS: u32 = 60;
     const THREE_HOURS_IN_MINS: u32 = 3 * ONE_HOUR_IN_MINS;
     println!("A const: {THREE_HOURS_IN_MINS}");
+
+    // Example of var shadowing
+    let x = 5;
+    let x = x + 1; // previous x is shadowed
+
+    {
+        let x = x * 2; // x from outer scope is shadowed
+        println!("The value in the inner scope is: {x}");
+    }
+
+    // in the outer scope the variable takes the previous value
+    println!("The value in the outer scope is: {x}");
 }
