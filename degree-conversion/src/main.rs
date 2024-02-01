7fb8d2187eb7e1023a0933894509ce3e078fb0a0
Pedro Juan Royo <pedro.juan.royo@gmail.com>
2024-02-01 18:36:53 +0000

    add dialoguer crate for user input

--- degree-conversion/src/main.rs
+++ degree-conversion/src/main.rs
@@ -1,4 +1,5 @@
-use std::io;
+use dialoguer::{Select, Input};
+use dialoguer::theme::ColorfulTheme;
 
 fn celsius_farenheit(deg: f32) -> f32 {
     deg*1.8 + 32.0
@@ -10,34 +11,42 @@ fn farenheit_celsius(deg: f32) -> f32 {
 
 fn main() {
     loop {
-        println!("Enter a value:");
+        // println!("Enter a value:");
 
-        let mut degree = String::new();
+        // let mut degree = String::new();
 
-        io::stdin().read_line(&mut degree).expect("failed to get value.");
+        // io::stdin().read_line(&mut degree).expect("failed to get value.");
+
+        // let degree: f32 = match degree.trim().parse() {
+        //     Ok(val) => val,
+        //     Err(_) => continue
+        // };
+
+        let degree: String = Input::with_theme(&ColorfulTheme::default()).with_prompt("Enter a value")
+            .validate_with(|input: &String| -> Result<(), &str> {
+                match input.trim().parse::<f32>() {
+                    Ok(_) => Ok(()),
+                    Err(_) => Err("Not valid value") 
+                }
+            }).interact_text().unwrap();
 
         let degree: f32 = match degree.trim().parse() {
             Ok(val) => val,
             Err(_) => continue
         };
 
-        println!("What conversion do you want? (F to C, C to F)");
+        let select_opts = vec!["Celsius to Farenheit", "Farenheit to Celsius"];
 
-        let mut conversion = String::new();
-
-        io::stdin().read_line(&mut conversion).expect("Failed to get value.");
+        let selection = Select::with_theme(&ColorfulTheme::default()).with_prompt("Select the conversion type").default(0)
+            .items(&select_opts).interact().unwrap();
 
         let result: f32;
+        match selection {
+            0 => result = celsius_farenheit(degree),
+            1 => result = farenheit_celsius(degree),
+            _ => result = 0.0
+        }
 
-        match conversion.as_str().trim() {
-            "F to C" => result = farenheit_celsius(degree),
-            "C to F" => result = celsius_farenheit(degree),
-            _ => {
-                println!("Unrecognised conversion.");
-                continue;
-            }
-        };
-
-        println!("The result is: {result}");
+        println!("The conversion to other degree scale is: {result}");
     }
 }
