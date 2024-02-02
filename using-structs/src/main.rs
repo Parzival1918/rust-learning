fcd8dd3812561fbb46a03073b6168cd012df7b79
Pedro Juan Royo <pedro.juan.royo@gmail.com>
2024-02-02 18:48:04 +0000

    Add intersect method to Line struct

--- using-structs/src/main.rs
+++ using-structs/src/main.rs
@@ -44,6 +44,26 @@ impl Line {
     fn calc_x(&self, y: f32) -> f32 {
         (y - self.c) / self.m
     }
+
+    fn intersect(&self, other: &Line) -> Result<Point, &str> {
+        if self.m == other.m && self.c == other.c {
+            return Err("Lines are equal, overlapping")
+        }
+
+        if self.m == other.m && self.c != other.c {
+            return Err("Lines are parallel, they do not overlap")
+        }
+
+        let x = (other.c - self.c) / (self.m - other.m);
+        let y = self.m*x + self.c;
+
+        let p = Point {
+            x,
+            y
+        };
+
+        Ok(p)
+    }
 }
 
 fn main() {
@@ -63,4 +83,16 @@ fn main() {
     println!("A line from origin to p: {:#?}", l);
     println!("Line y at x=10 is: {}", l.calc_y(10.0));
     println!("Line x at y=5 is: {}", l.calc_x(5.0));
+
+    let l2 = Line {
+        m: -2.0,
+        c: 4.0
+    };
+
+    let intersect = l.intersect(&l2);
+
+    match intersect {
+        Ok(p) => println!("The lines intersect at: {:?}", p),
+        Err(err) => println!("Lines do not intersect: {err}")
+    }
 }
